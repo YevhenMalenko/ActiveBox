ddd6b61a6cfec5bb2f5f71e483f595c6bda424e2
YevhenMalenko <38867295+YevhenMalenko@users.noreply.github.com>
2020-03-30 16:13:29 +0300

    test

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,38 +1,38 @@
 const test = () => {
 
-let featuresItems = document.querySelector('.features__items');
+  let featuresItems = document.querySelector('.features__items');
 
-let request = new XMLHttpRequest(),
+  let request = new XMLHttpRequest(),
     url = "assets/database/features.json";
 
-function createItem(src, title, desc) {
+  function createItem(src, title, desc) {
 
-  let div = document.createElement('div');
-  div.classList.add('features__item');
-  let itemContent =
-    `
+    let div = document.createElement('div');
+    div.classList.add('features__item');
+    let itemContent =
+      `
     <img class="features__item-img" src="${src}" alt="feature">
     <h3 class="features__item-title">${title}</h3>
     <div class="features__item-text">${desc}</div>
   `;
-  div.insertAdjacentHTML('afterbegin', itemContent);
-	return div
-}
+    div.insertAdjacentHTML('afterbegin', itemContent);
+    return div
+  }
 
-request.open("GET", url, true);
-request.setRequestHeader("content-type", "application/json");
-request.onreadystatechange = function() {
-	if (request.readyState == 4 && request.status == 200) {
-    let featuresArr = JSON.parse(request.responseText);
-    console.log(featuresArr);  
-		
-		featuresArr.forEach(function (item) {
-			featuresItems.appendChild(createItem(item.img, item.title, item.description ));
-		});
+  request.open("GET", url, true);
+  request.setRequestHeader("content-type", "application/json");
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      let featuresArr = JSON.parse(request.responseText);
+      console.log(featuresArr);
 
-	}
-};
-request.send();
+      featuresArr.forEach(function (item) {
+        featuresItems.appendChild(createItem(item.img, item.title, item.description));
+      });
+
+    }
+  };
+  request.send();
 
 };
 
